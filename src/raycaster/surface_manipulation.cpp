c4f420da31635d5a36039906d8450609700a709f
Michael Hitchens <mdhitchens@gmail.com>
2018-09-02 14:38:49 -0400

    Force format to be BGR24
    Differs from BGR888 in that the values can be red as 3-tuples

--- src/raycaster/surface_manipulation.cpp
+++ src/raycaster/surface_manipulation.cpp
@@ -10,21 +10,23 @@ using namespace mymath;
 namespace {
 
 constexpr auto desired_bpp = 3;
+constexpr auto desired_format = SDL_PIXELFORMAT_BGR24;
 
 bool is_surface_of_desired_format(
-    SDL_Surface* surf, int want_bpp)
+    SDL_Surface* surf, int want_bpp, Uint32 want_format)
 {
-    return surf->format->BytesPerPixel == want_bpp;
+    return surf->format->BytesPerPixel == want_bpp
+        && surf->format->format == want_format;
 }
 
 color get_surface_pixel(SDL_Surface* surf, point2i const& p)
 {
-    if (!is_surface_of_desired_format(surf, desired_bpp)) {
-	SDL_Log("Invalid surface format: 0x%x", surf->format->format);
+    if (!is_surface_of_desired_format(surf, desired_bpp, desired_format)) {
+        SDL_Log("Invalid surface format: 0x%x", surf->format->format);
         throw std::runtime_error{"Got invalid bitmap surface! See log"};
     }
 
-    // Assuming BGR888
+    // Assuming BGR24
     auto const index = p.y * surf->pitch + p.x * surf->format->BytesPerPixel;
     auto const pixel = static_cast<Uint8*>(surf->pixels) + index;
     return color{pixel[2], pixel[1], pixel[0]};
