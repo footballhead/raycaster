1d696dfe1e8ce917c15b0580c9f3ba53de6e04e4
Michael Hitchens <mdhitchens@gmail.com>
2019-01-01 14:17:57 -0500

    Keep building up sample

--- src/luatest/test.cpp
+++ src/luatest/test.cpp
@@ -1,7 +1,23 @@
-#include <iostream>
+#include <lua.h>
+#include <lualib.h>
+#include <lauxlib.h>
+
+#include <stdexcept>
 
 int main(int /*argc*/, char** /*argv*/)
 {
-    std::cout << "Hello world" << std::endl;
+    auto L = luaL_newstate();
+    if (!L) {
+        throw std::runtime_error{"luaL_newstate: not enough memory"};
+    }
+
+    luaL_openlibs(L);
+
+    if (!luaL_dostring(L, "io.stdout:write(5)")) {
+        lua_close(L);
+        throw std::runtime_error{"luaL_dostring: failed!"};
+    }
+
+    lua_close(L);
     return 0;
 }
