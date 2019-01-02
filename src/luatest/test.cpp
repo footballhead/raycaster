d7c286551ea6cdba50eface2f72e0ad8b75e7071
Michael Hitchens <mdhitchens@gmail.com>
2019-01-01 21:29:15 -0500

    Add lua_raii

--- src/luatest/test.cpp
+++ src/luatest/test.cpp
@@ -1,25 +1,17 @@
-#include <lua.h>
-#include <lualib.h>
-#include <lauxlib.h>
+#include <lua_raii/lua_raii.hpp>
 
 #include <stdexcept>
 #include <string>
 
 int main(int /*argc*/, char** /*argv*/)
 {
-    auto L = luaL_newstate();
-    if (!L) {
-        throw std::runtime_error{"luaL_newstate: not enough memory"};
-    }
+    auto L = lua::make_state();
 
-    luaL_openlibs(L);
-
-    if (luaL_dostring(L, R"(print("hello world"))")) {
-        auto const lua_error = std::string{lua_tostring(L, lua_gettop(L))};
-        lua_close(L);
+    if (luaL_dostring(L.get(), R"(print("hello world"))")) {
+        auto const lua_error
+            = std::string{lua_tostring(L.get(), lua_gettop(L.get()))};
         throw std::runtime_error{lua_error};
     }
 
-    lua_close(L);
     return 0;
 }
